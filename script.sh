f55e4109020562f5c186c9ae15c61380ed8edbb8
AdrianoVolter <adriano.j.volter@hotmail.com>
2021-10-03 18:39:21 -0300

    script.sh

---
+++ script.sh
@@ -0,0 +1,17 @@
+#! /bin/bash 
+echo "***********************"
+echo "Oi como  vcs estao tudo bem"
+echo "****************"
+echo
+echo
+echo " Lista"
+cat AULA.por
+echo
+echo
+echo "Historico de comandos"
+history
+free -g
+free
+echo
+echo "-"
+echo "Fim de execuçao"
